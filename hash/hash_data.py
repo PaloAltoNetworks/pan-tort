3ebef4f9d9469de7f38bcddc29c9d6ee6d8bf328
scottshoaf <sshoaf@paloaltonetworks.com>
2018-02-07 15:52:46 -0500

    minor formatting edits

--- hash/hash_data.py
+++ hash/hash_data.py
@@ -1,34 +1,34 @@
 ####!/usr/bin/env python3
+"""
+hash_data reads a list of md5 hash strings and performs 2 Autofocus api
+queries to get verdict/filetype and then signature coverage data
+This provides contextual information in test environments beyond just a hash miss
+"""
 import sys
 import os
 import os.path
 import json
-import time
-import re
 
 from panafapi_hacked import panafapi_hacked
-from panrc import *
+from panrc import hostname, api_key
 
 def main():
 
+    """hash_data main module"""
+
     libpath = os.path.dirname(os.path.abspath(__file__))
     sys.path[:0] = [os.path.join(libpath, os.pardir, 'lib')]
-    thismodule = sys.modules[__name__]
 
-
-    malware_values = {
-                        '0': 'benign',
-                        '1': 'malware',
-                        '2': 'grayware'
-                    }
+    malware_values = {'0': 'benign', '1': 'malware', '2': 'grayware'}
 
     AFoutput_dict = {}
     hashList = []
 
- # Use counters to create a simple output stats file in tandem with json details   
+ # Use counters to create a simple output stats file in tandem with json details
 
     HashCounters = {}
-    HashCountValues = ['total samples', 'malware','mal_inactive_sig', 'mal_active_sig', 'grayware', 'benign', 'No sample found']
+    HashCountValues = ['total samples', 'malware', 'mal_inactive_sig', 'mal_active_sig',
+                       'grayware', 'benign', 'No sample found']
 
     for value in HashCountValues:
         HashCounters[value] = 0
@@ -48,7 +48,7 @@ def main():
 # read the hash list file - a simple text list of md5 hashes
 # the text file is converted to a simple python list
 
-    with open('hash_list.txt','r') as hashFile:
+    with open('hash_list.txt', 'r') as hashFile:
         hashList = hashFile.read().splitlines()
 
 # query each md5 hash to get malware verdict
@@ -83,16 +83,23 @@ def main():
 # Print each coverage section to the screen - can comment out the print statements
 
             print('\nSearching Autofocus for current signature coverage...\n')
-            af_output = panafapi_hacked(hostname, api_key, 'sample_analysis', hash_data_dict['sha256hash'])
+            af_output = panafapi_hacked(hostname, api_key, 'sample_analysis',
+                                        hash_data_dict['sha256hash'])
             AFoutput_analysis = json.loads(af_output)
 
-            print('\nDNS Sig coverage: \n' + json.dumps(AFoutput_analysis['coverage']['dns_sig'], indent=4, sort_keys=False))
+            print('\nDNS Sig coverage: \n' +
+                  json.dumps(AFoutput_analysis['coverage']['dns_sig'],
+                             indent=4, sort_keys=False))
             hash_data_dict['dns_sig'] = AFoutput_analysis['coverage']['dns_sig']
 
-            print('\nWF_AV Sig coverage: \n' + json.dumps(AFoutput_analysis['coverage']['wf_av_sig'], indent=4, sort_keys=False))
+            print('\nWF_AV Sig coverage: \n' +
+                  json.dumps(AFoutput_analysis['coverage']['wf_av_sig'],
+                             indent=4, sort_keys=False))
             hash_data_dict['wf_av_sig'] = AFoutput_analysis['coverage']['wf_av_sig']
-            
-            print('\nFile URL Sig coverage: \n' + json.dumps(AFoutput_analysis['coverage']['fileurl_sig'], indent=4, sort_keys=False))
+
+            print('\nFile URL Sig coverage: \n' +
+                  json.dumps(AFoutput_analysis['coverage']['fileurl_sig'],
+                             indent=4, sort_keys=False))
             hash_data_dict['fileurl_sig'] = AFoutput_analysis['coverage']['fileurl_sig']
 
 # Check all the sig states [true or false] to see active vs inactive sigs for malware
@@ -100,11 +107,11 @@ def main():
             if verdict_text == 'malware':
                 sigSearch = json.dumps(hash_data_dict)
                 if sigSearch.find('true') == -1:
-                    HashCounters['mal_inactive_sig'] +=1
+                    HashCounters['mal_inactive_sig'] += 1
                 else:
-                    HashCounters['mal_active_sig'] +=1                   
+                    HashCounters['mal_active_sig'] += 1
 
- 
+
 # If no hash found then tag as 'no sample found'
 # These hashes can be check in VirusTotal to see if unsupported file type for Wildfire
 
@@ -113,8 +120,8 @@ def main():
             print('\nNo sample found in Autofocus for this hash')
             verdict_text = 'No sample found'
 
-            
-        HashCounters[verdict_text] +=1
+
+        HashCounters[verdict_text] += 1
 
 
 # write hash data to text file; for index = 1 create new file; for index > 1 append to file
@@ -122,7 +129,7 @@ def main():
 # hash_data_pretty has readable formatting to view the raw hash context data
 
         if index == 1:
-            with open('hash_data_estack.json','w') as hashFile:
+            with open('hash_data_estack.json', 'w') as hashFile:
                 hashFile.write(json.dumps(index_tag_full, indent=None, sort_keys=False) + "\n")
                 hashFile.write(json.dumps(hash_data_dict, indent=None, sort_keys=False) + "\n")
 
@@ -130,7 +137,7 @@ def main():
                 hashFile.write(json.dumps(hash_data_dict, indent=4, sort_keys=False) + "\n")
 
         else:
-            with open('hash_data_estack.json','a') as hashFile:
+            with open('hash_data_estack.json', 'a') as hashFile:
                 hashFile.write(json.dumps(index_tag_full, indent=None, sort_keys=False) + "\n")
                 hashFile.write(json.dumps(hash_data_dict, indent=None, sort_keys=False) + "\n")
 
@@ -145,7 +152,7 @@ def main():
         print('\nCurrent hash count stats:\n')
         print(json.dumps(HashCounters, indent=4, sort_keys=False) + '\n')
         with open('hash_data_stats.json', 'w') as hashFile:
-                hashFile.write(json.dumps(HashCounters, indent=4, sort_keys=False) + "\n")
+            hashFile.write(json.dumps(HashCounters, indent=4, sort_keys=False) + "\n")
 
 
         index += 1
@@ -153,4 +160,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
