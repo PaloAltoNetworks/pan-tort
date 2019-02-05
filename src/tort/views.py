33281d014f76893f40ea099fe776c155a43156b6
Edward Arcuri <edward.arcuri@gmail.com>
2019-02-05 15:10:04 -0700

    wtf

--- src/tort/views.py
+++ src/tort/views.py
@@ -1,5 +1,6 @@
 
 from pan_cnc.views import CNCBaseAuth, CNCBaseFormView, ProvisionSnippetView
+from pan_cnc.lib import cnc_utils
 import requests
 
 class tortView(CNCBaseFormView):
@@ -9,7 +10,7 @@ class tortView(CNCBaseFormView):
 
        # define initial dynamic form from this snippet metadata
         snippet = 'run_tort'
-        next_url = '/pantort/process_hashes'
+        # next_url = '/pantort/process_hashes'
 
         def get_snippet(self):
             return self.snippet
@@ -23,10 +24,35 @@ class tortView(CNCBaseFormView):
         hashes = workflow.get('hashes')
         output_type = workflow.get('output_type')
         payload = {'query_tag': query_tag,'hashes': hashes, 'output_type': output_type}
+        tortHost = cnc_utils.get_config_value("TORT_HOST","localhost")
+        tortPort = cnc_utils.get_config_value("TORT_PORT", 5010)
         
-        r = requests.post(f'http://{tortHost}:{tortPort}', data=payload)
+        resp = requests.post(f'http://{tortHost}:{tortPort}', data=payload)
+        print(resp.headers)
 
-        
-        print('set device-group and template to firewall name')
+        if resp.status_code == 200:
+            if 'json' in content_type:
+                return_json = resp.json()
+                if 'response' in return_json:
+                    result_text = return_json["response"]
+                else:
+                    result_text = resp.text
 
-        return super().form_valid(form)
+                results = dict()
+                results['results'] = str(resp.status_code)
+                results['results'] += '\n'
+                results['results'] += result_text
+                return render(self.request, 'pan_cnc/results.html', context=results)
+
+            else:
+                response = HttpResponse(content_type=content_type)
+                response['Content-Disposition'] = 'attachment; filename=%s' % filename
+                response.write(resp.content)
+                return response
+        else:
+            results = super().get_context_data()
+            results['results'] = str(resp.status_code)
+            results['results'] += '\n'
+            results['results'] += resp.text
+
+            return render(self.request, 'pan_cnc/results.html', context=results)
